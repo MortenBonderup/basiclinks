7a0d75c80f76e82ab87bec497f76f6bf062252d7
Morten Bonderup <moab@eaaa.dk>
2025-02-07 15:42:07 +0100

    version16

--- src/FirstPage.jsx
+++ src/FirstPage.jsx
@@ -1,8 +1,10 @@
+import fish from "./assets/fightingfish.jpg";
 function FirstPage() {
     return (
         <>
             <h1>First</h1>
             <p>Content on first page</p>
+            <img src={fish} alt="Fish" style={{ width: "300px" }} />
         </>
     )
 }
